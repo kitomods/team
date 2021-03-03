0eba84ed35d6e9cc28082282c25b3e88e25d6a93
kitomods <78054628+kitomods@users.noreply.github.com>
2021-03-03 16:55:54 -0300

    update index.js

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Iae @${num.split('@')[0]}\n bem vindo ao grupo ${mdata.subject}`
+				teks = `Iae @${num.split('@')[0]}\n bem vindo ao grupo\n ${mdata.subject}`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
